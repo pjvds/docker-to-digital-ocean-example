dea5b1265413e7fa3eed0c2dcd4be71336bce07c
Pieter Joost van de Sande <pj@born2code.net>
2013-10-01 20:48:49 +0200

    Host on direct domain

--- main.go
+++ main.go
@@ -7,19 +7,22 @@ import (
 	"github.com/gorilla/mux"
 	"net/http"
 	"os"
+	"strings"
+	"sync"
 )
 
 var (
 	counters = make(map[string]uint64)
 	host     = flag.String("host", "localhost", "the host to bind on")
 	port     = flag.Int("port", 8080, "the port to bind on")
+	mutex    sync.Mutex
 )
 
 func main() {
 	flag.Parse()
 
 	counterRouter := mux.NewRouter()
-	counterRouter.HandleFunc("/counter/{name}", CounterHandler)
+	counterRouter.HandleFunc("/{name}", CounterHandler)
 
 	address := fmt.Sprintf("%v:%v", *host, *port)
 
@@ -33,13 +36,19 @@ func CounterHandler(response http.ResponseWriter, request *http.Request) {
 	vars := mux.Vars(request)
 	name := vars["name"]
 
+	fmt.Fprintf(response, "%v", count(name))
+}
+
+func count(name string) {
+	lower := strings.ToLower(name)
+	mutex.Lock()
+	defer mutex.Unlock()
+
 	var count uint64
-	if current, ok := counters[name]; ok {
+	if current, ok := counters[lower]; ok {
 		count = current + 1
 	} else {
 		count = 1
 	}
-
-	counters[name] = count
-	fmt.Fprintf(response, "%v", count)
+	counters[lower] = count
 }
